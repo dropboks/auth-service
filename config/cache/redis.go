e6322e61a13c06e5c4879c3d1cdc984039de8ddf
daffadon <daffaputranarendra9@gmail.com>
2025-06-21 22:04:06 +0700

    chore(env): move properties to config file

--- config/cache/redis.go
+++ config/cache/redis.go
@@ -2,17 +2,20 @@ package cache
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/redis/go-redis/v9"
 	"github.com/rs/zerolog"
+	"github.com/spf13/viper"
 )
 
 func New(zerolog zerolog.Logger) (*redis.Client, error) {
+	addr := fmt.Sprintf("%s:%s", viper.GetString("redis.address"), viper.GetString("redis.port"))
 	client := redis.NewClient(&redis.Options{
-		Addr:       "localhost:6379",
-		ClientName: "auth_service",
+		Addr:       addr,
+		ClientName: viper.GetString("redis.client_name"),
 		Protocol:   2,
-		Password:   "",
+		Password:   viper.GetString("redis.password"),
 	})
 
 	ctx := context.Background()
