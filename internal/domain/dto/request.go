c218e22d0c431c42b47aa5cd3e6170a041e5dd95
daffadon <daffaputranarendra9@gmail.com>
2025-06-30 10:52:09 +0700

    refactor(type): add validation to input

--- internal/domain/dto/request.go
+++ internal/domain/dto/request.go
@@ -4,21 +4,21 @@ import "mime/multipart"
 
 type (
 	RegisterRequest struct {
-		FullName        string                `form:"full_name" binding:"required"`
-		Image           *multipart.FileHeader `form:"image" binding:"required"`
-		Email           string                `form:"email" binding:"required"`
-		Password        string                `form:"password" binding:"required"`
-		ConfirmPassword string                `form:"confirm_password" binding:"required"`
+		FullName        string                `form:"full_name" binding:"required,min=1,max=100"`
+		Image           *multipart.FileHeader `form:"image" `
+		Email           string                `form:"email" binding:"required,email"`
+		Password        string                `form:"password" binding:"required,min=8"`
+		ConfirmPassword string                `form:"confirm_password" binding:"required,min=8"`
 	}
 	LoginRequest struct {
-		Email    string `json:"email" binding:"required"`
-		Password string `json:"password" binding:"required"`
+		Email    string `json:"email" binding:"required,email"`
+		Password string `json:"password" binding:"required,min=6"`
 	}
 	ResendVerificationRequest struct {
-		Email string `json:"email" binding:"required"`
+		Email string `json:"email" binding:"required,email"`
 	}
 	VerifyOTPRequest struct {
-		Email string `json:"email" binding:"required"`
-		OTP   string `json:"otp" binding:"required"`
+		Email string `json:"email" binding:"required,email"`
+		OTP   string `json:"otp" binding:"required,len=6"`
 	}
 )
