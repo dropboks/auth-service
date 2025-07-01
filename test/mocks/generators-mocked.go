29c8100a779620209c69c83f2cc1bba496010a31
daffadon <daffaputranarendra9@gmail.com>
2025-07-01 15:41:46 +0700

    test(mock): add mock random generator, repository, and service

---
+++ test/mocks/generators-mocked.go
@@ -0,0 +1,17 @@
+package mocks
+
+import "github.com/stretchr/testify/mock"
+
+type MockRandomGenerator struct {
+	mock.Mock
+}
+
+func (m *MockRandomGenerator) GenerateUUID() string {
+	args := m.Called()
+	return args.String(0)
+}
+
+func (m *MockRandomGenerator) GenerateToken() (string, error) {
+	args := m.Called()
+	return args.String(0), args.Error(1)
+}
--- test/mocks/repository-mocked.go
+++ test/mocks/repository-mocked.go
@@ -1,1 +1,27 @@
 package mocks
+
+import (
+	"context"
+	"time"
+
+	m "github.com/stretchr/testify/mock"
+)
+
+type MockAuthRepository struct {
+	m.Mock
+}
+
+func (m *MockAuthRepository) GetResource(ctx context.Context, s string) (string, error) {
+	args := m.Called(ctx, s)
+	return args.String(0), args.Error(1)
+}
+
+func (m *MockAuthRepository) SetResource(ctx context.Context, s1 string, s2 string, t time.Duration) error {
+	args := m.Called(ctx, s1, s2, t)
+	return args.Error(0)
+}
+
+func (m *MockAuthRepository) RemoveResource(ctx context.Context, s string) error {
+	args := m.Called(ctx, s)
+	return args.Error(0)
+}
