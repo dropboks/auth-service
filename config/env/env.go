b9d573c0dc01872d25749a85d621a95dd2e40bf7
daffadon <daffaputranarendra9@gmail.com>
2025-07-03 11:32:05 +0700

    chore(env): add env load for test env

--- config/env/env.go
+++ config/env/env.go
@@ -9,12 +9,17 @@ import (
 func Load() {
 	env := os.Getenv("ENV")
 	configName := "config"
-	if env != "production" {
+	configpath := "."
+	switch env {
+	case "production":
 		configName = "config.local"
+	case "test":
+		configName = "config.test"
+		configpath = "../../"
 	}
 	viper.SetConfigName(configName)
 	viper.SetConfigType("yaml")
-	viper.AddConfigPath(".")
+	viper.AddConfigPath(configpath)
 	if err := viper.ReadInConfig(); err != nil {
 		panic("failed to read config")
 	}
