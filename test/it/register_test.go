724679d1b85ad6ad4725f0d1d2e811999bb803d6
daffadon <daffaputranarendra9@gmail.com>
2025-07-04 09:12:50 +0700

    test(IT): add IT for register and verify

--- test/it/register_test.go
+++ test/it/register_test.go
@@ -1,1 +1,332 @@
 package service_test
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"os/exec"
+	"syscall"
+	"testing"
+
+	"github.com/dropboks/auth-service/cmd/bootstrap"
+	"github.com/dropboks/auth-service/cmd/server"
+	"github.com/dropboks/auth-service/config/env"
+	"github.com/dropboks/auth-service/internal/infrastructure/grpc"
+	"github.com/dropboks/auth-service/test/helper"
+	"github.com/dropboks/proto-user/pkg/upb"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/suite"
+	"github.com/testcontainers/testcontainers-go"
+)
+
+type RegisterITSuite struct {
+	suite.Suite
+	ctx context.Context
+
+	network              *testcontainers.DockerNetwork
+	pgContainer          *helper.PostgresContainer
+	redisContainer       *helper.RedisContainer
+	minioContainer       *helper.MinioContainer
+	natsContainer        *helper.NatsContainer
+	userServiceContainer *helper.UserServiceContainer
+	fileServiceContainer *helper.FileServiceContainer
+
+	userServiceClient upb.UserServiceClient
+}
+
+func (r *RegisterITSuite) SetupSuite() {
+	exec.Command("docker", "rm", "-f", "db").Run()
+	log.Println("Setting up integration test suite for RegisterITSuite")
+	r.ctx = context.Background()
+	gin.SetMode(gin.TestMode)
+	os.Setenv("ENV", "test")
+	env.Load()
+
+	// spawn sharedNetwork
+	r.network = helper.StartNetwork(r.ctx)
+
+	// spawn posgresql
+	pgContainer, err := helper.StartPostgresContainer(r.ctx, r.network.Name)
+	if err != nil {
+		log.Fatalf("failed starting postgres container: %s", err)
+	}
+	r.pgContainer = pgContainer
+
+	// spawn redis
+	rContainer, err := helper.StartRedisContainer(r.ctx, r.network.Name)
+	if err != nil {
+		log.Fatalf("failed starting redis container: %s", err)
+	}
+	r.redisContainer = rContainer
+
+	// spawn minio
+	mContainer, err := helper.StartMinioContainer(r.ctx, r.network.Name)
+	if err != nil {
+		log.Fatalf("failed starting minio container: %s", err)
+	}
+	r.minioContainer = mContainer
+
+	// spawn nats
+	nContainer, err := helper.StartNatsContainer(r.ctx, r.network.Name)
+	if err != nil {
+		log.Fatalf("failed starting minio container: %s", err)
+	}
+	r.natsContainer = nContainer
+
+	// spawn user service
+	uContainer, err := helper.StartUserServiceContainer(r.ctx, r.network.Name)
+	if err != nil {
+		log.Println("make sure the image is exist")
+		log.Fatalf("failed starting user service container: %s", err)
+	}
+	r.userServiceContainer = uContainer
+
+	// spawn file service
+	fContainer, err := helper.StartFileServiceContainer(r.ctx, r.network.Name)
+	if err != nil {
+		log.Println("make sure the image is exist")
+		log.Fatalf("failed starting file service container: %s", err)
+	}
+	r.fileServiceContainer = fContainer
+
+	grpcManager := grpc.NewGRPCClientManager()
+	r.userServiceClient = grpc.NewUserServiceConnection(grpcManager)
+
+	container := bootstrap.Run()
+	serverReady := make(chan bool)
+	server := &server.Server{
+		Container:   container,
+		ServerReady: serverReady,
+	}
+	go server.Run()
+	<-serverReady
+}
+func (r *RegisterITSuite) TearDownSuite() {
+	if err := r.pgContainer.Terminate(r.ctx); err != nil {
+		log.Fatalf("error terminating postgres container: %s", err)
+	}
+	if err := r.redisContainer.Terminate(r.ctx); err != nil {
+		log.Fatalf("error terminating redis container: %s", err)
+	}
+	if err := r.minioContainer.Terminate(r.ctx); err != nil {
+		log.Fatalf("error terminating minio container: %s", err)
+	}
+	if err := r.natsContainer.Terminate(r.ctx); err != nil {
+		log.Fatalf("error terminating nats container: %s", err)
+	}
+	if err := r.userServiceContainer.Terminate(r.ctx); err != nil {
+		log.Fatalf("error terminating user service container: %s", err)
+	}
+	if err := r.fileServiceContainer.Terminate(r.ctx); err != nil {
+		log.Fatalf("error terminating file service container: %s", err)
+	}
+
+	p, _ := os.FindProcess(syscall.Getpid())
+	_ = p.Signal(syscall.SIGINT)
+	log.Println("Tear Down integration test suite for RegisterITSuite")
+}
+func TestRegisterITSuite(t *testing.T) {
+	suite.Run(t, &RegisterITSuite{})
+}
+
+func (r *RegisterITSuite) TestRegisterIT_Success() {
+	reqBody := &bytes.Buffer{}
+
+	formWriter := multipart.NewWriter(reqBody)
+	_ = formWriter.WriteField("full_name", "test-full-name")
+	_ = formWriter.WriteField("email", "test@example.com")
+	_ = formWriter.WriteField("password", "password123")
+	_ = formWriter.WriteField("confirm_password", "password123")
+
+	fileWriter, _ := formWriter.CreateFormFile("image", "test.jpg")
+	_, err := fileWriter.Write([]byte("fake image data"))
+	r.NoError(err)
+	if err != nil {
+		log.Fatal("failed to create image data")
+	}
+	formWriter.Close()
+
+	request, err := http.NewRequest(http.MethodPost, "http://localhost:8181/register", reqBody)
+	request.Header.Set("Content-Type", formWriter.FormDataContentType())
+	r.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(request)
+	r.NoError(err)
+
+	byteBody, err := io.ReadAll(response.Body)
+	r.NoError(err)
+
+	r.Equal(http.StatusCreated, response.StatusCode)
+	r.Contains(string(byteBody), "Register Success. Check your email for verification.")
+	response.Body.Close()
+}
+
+func (r *RegisterITSuite) TestRegisterIT_MissingBody() {
+	reqBody := &bytes.Buffer{}
+
+	formWriter := multipart.NewWriter(reqBody)
+	_ = formWriter.WriteField("email", "test@example.com")
+	_ = formWriter.WriteField("password", "password123")
+	_ = formWriter.WriteField("confirm_password", "password123")
+
+	formWriter.Close()
+
+	request, err := http.NewRequest(http.MethodPost, "http://localhost:8181/register", reqBody)
+	request.Header.Set("Content-Type", formWriter.FormDataContentType())
+	r.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(request)
+	r.NoError(err)
+
+	byteBody, err := io.ReadAll(response.Body)
+	r.NoError(err)
+
+	r.Equal(http.StatusBadRequest, response.StatusCode)
+	r.Contains(string(byteBody), "invalid input")
+	response.Body.Close()
+}
+
+func (r *RegisterITSuite) TestRegisterIT_EmailAlreadyExist() {
+	imageName := "image-name"
+	user := &upb.User{
+		Id:               "user-id-1",
+		FullName:         "test-user",
+		Image:            &imageName,
+		Email:            "test1@example.com",
+		Password:         "$2a$10$Nwjs8PdFOCnjbRM3x/2WAuEtqOSrm6wHByYaw0ZDp5mV7e560dIb6",
+		Verified:         true,
+		TwoFactorEnabled: false,
+	}
+	_, err := r.userServiceClient.CreateUser(r.ctx, user)
+	r.NoError(err)
+
+	reqBody := &bytes.Buffer{}
+
+	formWriter := multipart.NewWriter(reqBody)
+	_ = formWriter.WriteField("full_name", "test-user")
+	_ = formWriter.WriteField("email", "test1@example.com")
+	_ = formWriter.WriteField("password", "password123")
+	_ = formWriter.WriteField("confirm_password", "password123")
+
+	formWriter.Close()
+
+	request, err := http.NewRequest(http.MethodPost, "http://localhost:8181/register", reqBody)
+	request.Header.Set("Content-Type", formWriter.FormDataContentType())
+	r.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(request)
+	r.NoError(err)
+
+	byteBody, err := io.ReadAll(response.Body)
+	r.NoError(err)
+
+	r.Equal(http.StatusConflict, response.StatusCode)
+	r.Contains(string(byteBody), "user with this email exist")
+	response.Body.Close()
+}
+
+func (r *RegisterITSuite) TestRegisterIT_WrongExtension() {
+	reqBody := &bytes.Buffer{}
+
+	formWriter := multipart.NewWriter(reqBody)
+	_ = formWriter.WriteField("full_name", "test-full-name")
+	_ = formWriter.WriteField("email", "test2@example.com")
+	_ = formWriter.WriteField("password", "password123")
+	_ = formWriter.WriteField("confirm_password", "password123")
+
+	fileWriter, _ := formWriter.CreateFormFile("image", "test.webp")
+	_, err := fileWriter.Write([]byte("fake image data"))
+	r.NoError(err)
+	if err != nil {
+		log.Fatal("failed to create image data")
+	}
+	formWriter.Close()
+
+	request, err := http.NewRequest(http.MethodPost, "http://localhost:8181/register", reqBody)
+	request.Header.Set("Content-Type", formWriter.FormDataContentType())
+	r.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(request)
+	r.NoError(err)
+
+	byteBody, err := io.ReadAll(response.Body)
+	r.NoError(err)
+
+	r.Equal(http.StatusBadRequest, response.StatusCode)
+	r.Contains(string(byteBody), "error file extension, support jpg, jpeg, and png")
+	response.Body.Close()
+}
+
+func (r *RegisterITSuite) TestRegisterIT_LimitSizeExceeded() {
+	reqBody := &bytes.Buffer{}
+
+	formWriter := multipart.NewWriter(reqBody)
+	_ = formWriter.WriteField("full_name", "test-full-name")
+	_ = formWriter.WriteField("email", "test3@example.com")
+	_ = formWriter.WriteField("password", "password123")
+	_ = formWriter.WriteField("confirm_password", "password123")
+	fileWriter, _ := formWriter.CreateFormFile("image", "test.png")
+	largeData := make([]byte, 8*1024*1024)
+	_, err := fileWriter.Write(largeData)
+	r.NoError(err)
+	if err != nil {
+		log.Fatal("failed to create image data")
+	}
+	formWriter.Close()
+
+	request, err := http.NewRequest(http.MethodPost, "http://localhost:8181/register", reqBody)
+	request.Header.Set("Content-Type", formWriter.FormDataContentType())
+	r.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(request)
+	r.NoError(err)
+
+	byteBody, err := io.ReadAll(response.Body)
+	r.NoError(err)
+
+	r.Equal(http.StatusBadRequest, response.StatusCode)
+	r.Contains(string(byteBody), "max size exceeded: 6mb")
+	response.Body.Close()
+}
+
+func (r *RegisterITSuite) TestRegisterIT_PasswordAndConfirmPasswordDoesntMatch() {
+	reqBody := &bytes.Buffer{}
+
+	formWriter := multipart.NewWriter(reqBody)
+	_ = formWriter.WriteField("full_name", "test-full-name")
+	_ = formWriter.WriteField("email", "test4@example.com")
+	_ = formWriter.WriteField("password", "password123")
+	_ = formWriter.WriteField("confirm_password", "password1234")
+
+	fileWriter, _ := formWriter.CreateFormFile("image", "test.jpg")
+	_, err := fileWriter.Write([]byte("fake image data"))
+	r.NoError(err)
+	if err != nil {
+		log.Fatal("failed to create image data")
+	}
+	formWriter.Close()
+
+	request, err := http.NewRequest(http.MethodPost, "http://localhost:8181/register", reqBody)
+	request.Header.Set("Content-Type", formWriter.FormDataContentType())
+	r.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(request)
+	r.NoError(err)
+
+	byteBody, err := io.ReadAll(response.Body)
+	r.NoError(err)
+
+	r.Equal(http.StatusBadRequest, response.StatusCode)
+	r.Contains(string(byteBody), "password and confirm password doesn't match")
+	response.Body.Close()
+}
--- test/it/verify_test.go
+++ test/it/verify_test.go
@@ -1,1 +1,275 @@
 package service_test
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"syscall"
+	"testing"
+
+	"github.com/dropboks/auth-service/cmd/bootstrap"
+	"github.com/dropboks/auth-service/cmd/server"
+	"github.com/dropboks/auth-service/config/env"
+	"github.com/dropboks/auth-service/internal/infrastructure/grpc"
+	"github.com/dropboks/auth-service/test/helper"
+	"github.com/dropboks/proto-user/pkg/upb"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/suite"
+	"github.com/testcontainers/testcontainers-go"
+)
+
+type VerifyITSuite struct {
+	suite.Suite
+	ctx context.Context
+
+	network              *testcontainers.DockerNetwork
+	pgContainer          *helper.PostgresContainer
+	redisContainer       *helper.RedisContainer
+	natsContainer        *helper.NatsContainer
+	userServiceContainer *helper.UserServiceContainer
+
+	userServiceClient upb.UserServiceClient
+}
+
+func (v *VerifyITSuite) SetupSuite() {
+	exec.Command("docker", "rm", "-f", "db").Run()
+	log.Println("Setting up integration test suite for VerifyITSuite")
+	v.ctx = context.Background()
+	gin.SetMode(gin.TestMode)
+	os.Setenv("ENV", "test")
+	env.Load()
+
+	// spawn sharedNetwork
+	v.network = helper.StartNetwork(v.ctx)
+
+	// spawn posgresql
+	pgContainer, err := helper.StartPostgresContainer(v.ctx, v.network.Name)
+	if err != nil {
+		log.Fatalf("failed starting postgres container: %s", err)
+	}
+	v.pgContainer = pgContainer
+
+	// spawn redis
+	rContainer, err := helper.StartRedisContainer(v.ctx, v.network.Name)
+	if err != nil {
+		log.Fatalf("failed starting redis container: %s", err)
+	}
+	v.redisContainer = rContainer
+
+	// spawn nats
+	nContainer, err := helper.StartNatsContainer(v.ctx, v.network.Name)
+	if err != nil {
+		log.Fatalf("failed starting minio container: %s", err)
+	}
+	v.natsContainer = nContainer
+
+	// spawn user service
+	uContainer, err := helper.StartUserServiceContainer(v.ctx, v.network.Name)
+	if err != nil {
+		log.Println("make sure the image is exist")
+		log.Fatalf("failed starting user service container: %s", err)
+	}
+	v.userServiceContainer = uContainer
+
+	grpcManager := grpc.NewGRPCClientManager()
+	v.userServiceClient = grpc.NewUserServiceConnection(grpcManager)
+
+	container := bootstrap.Run()
+	serverReady := make(chan bool)
+	server := &server.Server{
+		Container:   container,
+		ServerReady: serverReady,
+	}
+	go server.Run()
+	<-serverReady
+}
+func (v *VerifyITSuite) TearDownSuite() {
+	if err := v.pgContainer.Terminate(v.ctx); err != nil {
+		log.Fatalf("error terminating postgres container: %s", err)
+	}
+	if err := v.redisContainer.Terminate(v.ctx); err != nil {
+		log.Fatalf("error terminating redis container: %s", err)
+	}
+	if err := v.natsContainer.Terminate(v.ctx); err != nil {
+		log.Fatalf("error terminating nats container: %s", err)
+	}
+	if err := v.userServiceContainer.Terminate(v.ctx); err != nil {
+		log.Fatalf("error terminating user service container: %s", err)
+	}
+	p, _ := os.FindProcess(syscall.Getpid())
+	_ = p.Signal(syscall.SIGINT)
+	log.Println("Tear Down integration test suite for VerifyITSuite")
+}
+func TestVerifyITSuite(t *testing.T) {
+	suite.Run(t, &VerifyITSuite{})
+}
+
+func (v *VerifyITSuite) TestVerifyIT_Success() {
+	imageName := "image-name"
+	user := &upb.User{
+		Id:               "user-id-1",
+		FullName:         "test-user",
+		Image:            &imageName,
+		Email:            "test1@example.com",
+		Password:         "$2a$10$Nwjs8PdFOCnjbRM3x/2WAuEtqOSrm6wHByYaw0ZDp5mV7e560dIb6",
+		Verified:         true,
+		TwoFactorEnabled: false,
+	}
+	_, err := v.userServiceClient.CreateUser(v.ctx, user)
+	v.NoError(err)
+	reqBody := &bytes.Buffer{}
+
+	encoder := gin.H{
+		"email":    "test1@example.com",
+		"password": "password123",
+	}
+	_ = json.NewEncoder(reqBody).Encode(encoder)
+
+	req, err := http.NewRequest(http.MethodPost, "http://localhost:8181/login", reqBody)
+	v.NoError(err)
+
+	client := http.Client{}
+	response, err := client.Do(req)
+	v.NoError(err)
+
+	byteBody, err := io.ReadAll(response.Body)
+
+	v.NoError(err)
+	defer response.Body.Close()
+
+	var respData map[string]interface{}
+	err = json.Unmarshal(byteBody, &respData)
+	v.NoError(err)
+
+	jwt, ok := respData["data"].(string)
+	v.True(ok, "expected jwt token in data field")
+
+	verifyReq, err := http.NewRequest(http.MethodPost, "http://localhost:8181/verify", nil)
+	v.NoError(err)
+	verifyReq.Header.Set("Authorization", "Bearer "+jwt)
+
+	verifyResp, err := client.Do(verifyReq)
+	v.NoError(err)
+	defer verifyResp.Body.Close()
+
+	v.Equal(http.StatusNoContent, verifyResp.StatusCode)
+}
+
+func (v *VerifyITSuite) TestVerifyIT_MissingToken() {
+
+	verifyReq, err := http.NewRequest(http.MethodPost, "http://localhost:8181/verify", nil)
+	v.NoError(err)
+
+	client := http.Client{}
+	verifyResp, err := client.Do(verifyReq)
+	v.NoError(err)
+
+	byteBody, err := io.ReadAll(verifyResp.Body)
+	v.NoError(err)
+
+	v.Equal(http.StatusUnauthorized, verifyResp.StatusCode)
+	v.Contains(string(byteBody), "Token is missing")
+}
+
+func (v *VerifyITSuite) TestVerifyIT_InvalidFormat() {
+
+	verifyReq, err := http.NewRequest(http.MethodPost, "http://localhost:8181/verify", nil)
+	verifyReq.Header.Set("Authorization", "Bearer")
+	v.NoError(err)
+
+	client := http.Client{}
+	verifyResp, err := client.Do(verifyReq)
+	v.NoError(err)
+
+	byteBody, err := io.ReadAll(verifyResp.Body)
+	v.NoError(err)
+
+	v.Equal(http.StatusUnauthorized, verifyResp.StatusCode)
+	v.Contains(string(byteBody), "Invalid token format")
+}
+
+func (v *VerifyITSuite) TestVerifyIT_InvalidToken() {
+	jwt := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidXNlci1pZC0xMjMiLCJpYXQiOjE3NTEzNjEzOTJ9.CwkzkHgPYAxd6TXG4_ooMFczGvjn3Qr2_7T6W6YCDgI"
+
+	verifyReq, err := http.NewRequest(http.MethodPost, "http://localhost:8181/verify", nil)
+	verifyReq.Header.Set("Authorization", "Bearer "+jwt)
+	v.NoError(err)
+
+	client := http.Client{}
+	verifyResp, err := client.Do(verifyReq)
+	v.NoError(err)
+
+	byteBody, err := io.ReadAll(verifyResp.Body)
+	v.NoError(err)
+
+	v.Equal(http.StatusUnauthorized, verifyResp.StatusCode)
+	v.Contains(string(byteBody), "token is invalid")
+}
+
+func (v *VerifyITSuite) TestVerifyIT_UnmatchTokenWithTokenInTheState() {
+	imageName := "image-name"
+	user := &upb.User{
+		Id:               "user-id-2",
+		FullName:         "test-user2",
+		Image:            &imageName,
+		Email:            "test2@example.com",
+		Password:         "$2a$10$Nwjs8PdFOCnjbRM3x/2WAuEtqOSrm6wHByYaw0ZDp5mV7e560dIb6",
+		Verified:         true,
+		TwoFactorEnabled: false,
+	}
+	_, err := v.userServiceClient.CreateUser(v.ctx, user)
+	v.NoError(err)
+
+	reqBody1 := &bytes.Buffer{}
+	_ = json.NewEncoder(reqBody1).Encode(gin.H{
+		"email":    "test2@example.com",
+		"password": "password123",
+	})
+	req1, err := http.NewRequest(http.MethodPost, "http://localhost:8181/login", reqBody1)
+	v.NoError(err)
+
+	client := http.Client{}
+	resp1, err := client.Do(req1)
+	v.NoError(err)
+
+	defer resp1.Body.Close()
+	body1, err := io.ReadAll(resp1.Body)
+	v.NoError(err)
+
+	var respData1 map[string]interface{}
+	err = json.Unmarshal(body1, &respData1)
+	v.NoError(err)
+
+	jwt1, ok := respData1["data"].(string)
+	v.True(ok, "expected jwt token in data field")
+
+	reqBody2 := &bytes.Buffer{}
+	_ = json.NewEncoder(reqBody2).Encode(gin.H{
+		"email":    "test2@example.com",
+		"password": "password123",
+	})
+	req2, err := http.NewRequest(http.MethodPost, "http://localhost:8181/login", reqBody2)
+	v.NoError(err)
+
+	resp2, err := client.Do(req2)
+	v.NoError(err)
+	defer resp2.Body.Close()
+
+	verifyReq, err := http.NewRequest(http.MethodPost, "http://localhost:8181/verify", nil)
+	verifyReq.Header.Set("Authorization", "Bearer "+jwt1)
+	v.NoError(err)
+
+	verifyResp, err := client.Do(verifyReq)
+	v.NoError(err)
+
+	byteBody, err := io.ReadAll(verifyResp.Body)
+	v.NoError(err)
+
+	v.Equal(http.StatusUnauthorized, verifyResp.StatusCode)
+	v.Contains(string(byteBody), "token is invalid")
+}
